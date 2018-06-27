2c1990af7f7ae6ec35eb13956128534a0a901871
davew <dwoldrich_ebay@yahoo.com>
2018-06-27 06:36:07 -0700

    Delete dead code

--- packages/game/javascript/animationFrameObservable.js
+++ packages/game/javascript/animationFrameObservable.js
@@ -3,18 +3,6 @@
 var Rx = require("rxjs"),
   injector = require("jsuice");
 
-function AnimationFrameObservableFactory(animationFrameScheduler) {
-  /**
-   * @name AnimationFrameObservableFactory#animationFrameScheduler
-   * @type {Rx.Scheduler}
-   */
-  this.animationFrameScheduler = animationFrameScheduler;
-}
-
-AnimationFrameObservableFactory.prototype.create = function() {
-  return Rx.interval(0, this.animationFrameScheduler);
-};
-
 function animationFrameObservable(animationFrameScheduler) {
   return Rx.interval(0, animationFrameScheduler)
 }
