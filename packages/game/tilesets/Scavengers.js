33766a25aa8de0e35edff207dec3d5d22b2bf030
Joostin22 <steamsy22@gmail.com>
2018-07-21 15:16:51 -0800

    custom Tileset and map with layers

---
+++ packages/game/tilesets/Scavengers.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Steamsy on 7/21/2018.
+ */
+var tileDescriptors = [
+    {id : 0, name : "grass", walkable: true, seethrough : true},
+    {id : 1, name : "bricks", walkable: false, seethrough : false},
+    {id : 2, name : "roof", walkable : false, seethrough : false},
+];
+export default tileDescriptors;
