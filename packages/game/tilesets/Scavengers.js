05677333b2b05899fc14aa22ac8eb4af1149e4c4
davew <dave@woldrich.com>
2018-07-23 16:06:17 -0700

    First ground and roof of hometown, corrugated tin shanties

--- packages/game/tilesets/Scavengers.js
+++ packages/game/tilesets/Scavengers.js
@@ -10,8 +10,12 @@ var tileDescriptors = [
     {id : 5, name : "bush", walkable : false, seethrough : true},
     {id : 6, name : "mud", walkable : true, seethrough : true},
     {id : 7, name : "sand", walkable : true, seethrough : true},
-    {id : 8, name : "Wood_Floor", walkable : true, seethrough : true},
+    {id : 8, name : "Wood floor", walkable : true, seethrough : true},
     {id : 9, name : "Boulder", walkable : false, seethrough : true},
-
+    {id : 10, name : "Mountains", walkable : false, seethrough : true},
+    {id : 11, name : "Outdoor Lamp", walkable : true, seethrough : true},
+    {id : 12, name : "Indoor Lamp", walkable : false, seethrough : false},
+    {id : 13, name : "Shanty Walls", walkable : false, seethrough : false},
+    {id : 14, name : "Corrugated Tin Roofing", walkable : false, seethrough : false}
 ];
 export default tileDescriptors;
