328fa57944d541774326378e089a34b2c50e4904
Joostin22 <steamsy22@gmail.com>
2018-07-22 19:12:03 -0800

    I did more tiles and then there will be more tiles and then more tiles and then more not tiles.

--- packages/game/tilesets/Scavengers.js
+++ packages/game/tilesets/Scavengers.js
@@ -5,5 +5,10 @@ var tileDescriptors = [
     {id : 0, name : "grass", walkable: true, seethrough : true},
     {id : 1, name : "bricks", walkable: false, seethrough : false},
     {id : 2, name : "roof", walkable : false, seethrough : false},
+    {id : 3, name : "chimney", walkable : false, seethrough : false},
+    {id : 4, name : "water", walkable : false, seethrough : true},
+    {id : 5, name : "bush", walkable : false, seethrough : true},
+    {id : 6, name : "mud", walkable : true, seethrough : true},
+    {id : 7, name : "sand", walkable : true, seethrough : true},
 ];
 export default tileDescriptors;
